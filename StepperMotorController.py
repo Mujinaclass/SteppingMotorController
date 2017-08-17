eb8ba5ffc57fdb90fcb964d7cfd812ae4d6de12b
Yoshinori Suzuki (Pi) <suzuki.rikiken@gmail.com>
2017-08-17 18:29:02 -0400

    Add code to calculate flow-rate, and fix bugs.

--- StepperMotorController.py
+++ StepperMotorController.py
@@ -1,9 +1,6 @@
 import pigpio
 import time
 
-#GPIO27: STEP command
-#GPIO17: Direction CW:HIGH  CCW:LOW
-#GPIO22: Sleep mode
 # Microstep Resolution  |   M0   |  M1
 #         Full          |  LOW   | HIGH
 #         Half          |  HIGH  | LOW
@@ -14,28 +11,42 @@ import time
 # *Leaving selection pins disconnected results "Floating".
 #  Motor full step angle: 1.8 deg./step
 
-MOTOR_STEP_PIN = 27
-MOTOR_DIR_PIN = 17
-MOTOR_SLEEP_PIN = 22
+MOTOR_STEP_PIN = 27                #GPIO27: STEP command
+MOTOR_DIR_PIN = 17                 #GPIO17: Direction CW:HIGH  CCW:LOW
+MOTOR_SLEEP_PIN = 22               #GPIO22: Sleep mode
 MOTOR_M0_PIN = 24
 MOTOR_M1_PIN = 23
 MOTOR_STEP_ANGLE = 1.8
-MOTOR_PULSEDURATION_LIMIT = 1.9e-6 #[sec]
+MOTOR_SPEED_LIMIT = 5              # [rps] Stepping Motor limit is 5 rps.
 MOTOR_STEPRESMODES_DICT = {1: (0,0), 2: (1,0), 4: (0,0), 8: (0,1), 16: (1,1), 32: (0,1)} # Resolution: (M0 pin, M1 pin)
 
-SCREW_LEAD = 1.0
+SCREW_STANDARD = 'M4'
+SCREW_LEAD = 0.7                   # [mm] = pitch
 
-AVAIRABLE_PWM_FREQ = { 1: (50,100,200,250,400,500,800,1000,1250,1600,2000,2500,4000,5000,8000,10000,20000,40000), # Sample rate [us]: (Hertz)
-                       2: (25, 50,100,125,200,250,400, 500, 625, 800,1000,1250,2000,2500,4000, 5000,10000,20000), # Default is 5[us].
-                       4: (13, 25, 50, 63,100,125,200, 250, 313, 400, 500, 625,1000,1250,2000, 2500, 5000,10000),
-                       5: (10, 20, 40, 50, 80,100,160, 200, 250, 320, 400, 500, 800,1000,1600, 2000, 4000, 8000),
-                       8: ( 6, 13, 25, 31, 50, 63,100, 125, 156, 200, 250, 313, 500, 625,1000, 1250, 2500, 5000),
-                      10: ( 5, 10, 20, 25, 40, 50, 80, 100, 125, 160, 200, 250, 400, 500, 800, 1000, 2000, 4000)}
-# Stepping Motor limit is 2500Hz???
+SYRINGE_AREA = 165.13              # [mm^2]
 
-PWM_dutycycle = (0, 128) # PWM (STOP, 1/2 on)
-PWM_sample_rate = 8  # [us]
-motor_step_resolution = 2
+AVAIRABLE_PWM_FREQ_DICT = { 1: (50,100,200,250,400,500,800,1000,1250,1600,2000,2500,4000,5000,8000,10000,20000,40000), # Sample rate [us]: (Hertz)
+                            2: (25, 50,100,125,200,250,400, 500, 625, 800,1000,1250,2000,2500,4000, 5000,10000,20000),
+                            4: (13, 25, 50, 63,100,125,200, 250, 313, 400, 500, 625,1000,1250,2000, 2500, 5000,10000),
+                            5: (10, 20, 40, 50, 80,100,160, 200, 250, 320, 400, 500, 800,1000,1600, 2000, 4000, 8000),
+                            8: ( 6, 13, 25, 31, 50, 63,100, 125, 156, 200, 250, 313, 500, 625,1000, 1250, 2500, 5000),
+                           10: ( 5, 10, 20, 25, 40, 50, 80, 100, 125, 160, 200, 250, 400, 500, 800, 1000, 2000, 4000)}
+
+PWM_dutycycle = (0, 128)           # PWM (STOP, 1/2 on)
+PWM_sample_rate = 5                # Default is 5 us. If you want to change sample rate, type "sudo pigpiod -s <sample rate>" in comand line, when you start the pigpio deamon. 
+
+motor_step_resolution = 1
+motor_direction = 1                # direcion = 0 (CCW) or 1 (CW)
+
+AVAIRABLE_FLOW_RATE_DICT = {}      # {Flow rate [ml/s]: PWM frequency [Hz]}
+for freq in range(len(AVAIRABLE_PWM_FREQ_DICT[PWM_sample_rate])):
+    RPM = (1.8 / motor_step_resolution * AVAIRABLE_PWM_FREQ_DICT[PWM_sample_rate][freq]) / 360  # [rotation / sec]
+    flow_rate = RPM * 0.7 * SYRINGE_AREA * 10**-3 # [ml / sec]
+    if RPM <= MOTOR_SPEED_LIMIT:
+        AVAIRABLE_FLOW_RATE_DICT[flow_rate] = AVAIRABLE_PWM_FREQ_DICT[PWM_sample_rate][freq]
+    else:
+        thing = None               # do nothing
+
 
 def motorSettings(stepResolution):
     global pi, motor_step_resolution
@@ -46,8 +57,6 @@ def motorSettings(stepResolution):
     pi.set_mode(MOTOR_M0_PIN,pigpio.OUTPUT)
     pi.set_mode(MOTOR_M1_PIN,pigpio.OUTPUT)
 
-    pi.set_PWM_frequency(MOTOR_STEP_PIN, AVAIRABLE_PWM_FREQ[PWM_sample_rate][16])
-    pi.set_PWM_dutycycle(MOTOR_STEP_PIN, PWM_dutycycle[0])
     pi.write(MOTOR_SLEEP_PIN,1)
     time.sleep(1e-3)               #Wake-up time. Need to set over 1msec
 
@@ -55,7 +64,7 @@ def motorSettings(stepResolution):
         pi.write(MOTOR_M0_PIN,MOTOR_STEPRESMODES_DICT[stepResolution][0])
         pi.write(MOTOR_M1_PIN,MOTOR_STEPRESMODES_DICT[stepResolution][1])
     else:
-        motor_step_resolution = 1        # Set step resolution to Full-step mode
+        motor_step_resolution = 1  # Set step resolution to Full-step mode
         pi.write(MOTOR_M0_PIN,0)
         pi.write(MOTOR_M1_PIN,0)
         print("Invalid value. Step resolution should be within the following values.")
@@ -66,7 +75,12 @@ def motorSettings(stepResolution):
         print("Please disconnect M0 pin: GPIO" '%d' %MOTOR_M0_PIN)
 #end def motorSettings()
 
-def moveMotor(direc,vel,durat):      # direc = 0 (CCW) or 1 (CW), velocity [mm/sec], duration [sec]
+def motorFreqSetting():
+    pi.set_PWM_frequency(MOTOR_STEP_PIN, AVAIRABLE_PWM_FREQ_DICT[PWM_sample_rate][12])
+    pi.set_PWM_dutycycle(MOTOR_STEP_PIN, PWM_dutycycle[0])
+#end def motorFreqSetting()
+
+def moveMotor(direc,durat):
     pi.write(MOTOR_DIR_PIN,direc)
     pi.set_PWM_dutycycle(MOTOR_STEP_PIN, PWM_dutycycle[1])  # PWM on
     time.sleep(durat)
@@ -74,6 +88,7 @@ def moveMotor(direc,vel,durat):      # direc = 0 (CCW) or 1 (CW), velocity [mm/s
     
 
 motorSettings(motor_step_resolution)
-moveMotor(1,20,1)  #limit vel = 25? 20000Hz 50: 40000Hz
+motorFreqSetting()
+moveMotor(motor_direction,1)
 
 pi.write(MOTOR_SLEEP_PIN,0)
